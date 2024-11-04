a3996d9e8db541c87cecc0cd2a90de3a7e9bf505
Miguel Angel Gutierrez serrano <miguelangelgutierrezserrano@hotmail.com>
2024-11-03 23:50:03 -0500

    Add lab09 React project

---
+++ lab09/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Fullstack Development 1</h1>
+        <h2>React JS Programming Week09 Lab Exercise</h2>
+        <p>101449899</p>
+        <p>Miguel Angel Gutierrez</p>
+        <p>George Brown College</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
